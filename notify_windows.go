ecbb03a1f22a5426cd14338ed05321cc97364cb4
Hector Paz <hector.paz@clicksandbricks.pe>
2016-12-05 19:53:40 -0500

    add windows support

---
+++ notify_windows.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+//"github.com/ctcpip/notifize"
+)
+
+func NotifyDesktop() {
+
+	//notifize.Display("CodePicnic", "Console succesfully mounted", false, share_dir_linux+"/"+notify_file)
+}
---
+++ version_windows.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+//"bufio"
+//"os/exec"
+//"strings"
+)
+
+func GetOSVersion() string {
+	os_version := "Windows"
+	/*
+		var dist, release string
+		out, err := exec.Command("lsb_release", "-a").Output()
+		if err != nil {
+			os_version = "Linux"
+		} else {
+			scanner := bufio.NewScanner(strings.NewReader(string(out)))
+			for scanner.Scan() {
+				str_line := scanner.Text()
+				if strings.HasPrefix(str_line, "Distributor") {
+					dist = strings.Replace(str_line, "Distributor ID:\t", "", -1)
+					os_version = os_version + " " + dist
+				} else if strings.HasPrefix(str_line, "Release") {
+					release = strings.Replace(str_line, "Release:\t", "", -1)
+					os_version = os_version + " " + release
+				}
+			}
+		}*/
+	return os_version
+
+}
