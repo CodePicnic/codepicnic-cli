fd0e204ec092218e716c9b591b6a41c2edd8db83
Hector Paz <hector.paz@clicksandbricks.pe>
2017-01-05 14:46:36 -0500

    add interface to dokany

--- mount_windows.go
+++ mount_windows.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-
+	"github.com/keybase/kbfs/dokan"
 )
-
-
 
 func UnmountConsole(container_name string) error {
 
 	return nil
 }
 func MountConsole(access_token string, container_name string, mount_dir string) error {
-return nil
+	return nil
 }
