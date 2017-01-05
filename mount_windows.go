394832ee753c0da44dabdcb9cf7de247f239dde1
Hector Paz <hector.paz@clicksandbricks.pe>
2017-01-05 17:36:13 -0500

    update readme

--- mount_windows.go
+++ mount_windows.go
@@ -9,5 +9,14 @@ func UnmountConsole(container_name string) error {
 	return nil
 }
 func MountConsole(access_token string, container_name string, mount_dir string) error {
+	var myFileSystem FileSystem // Should be the real filesystem implementation
+	mp, err := Mount(&Config{FileSystem: myFileSystem, Path: `Q:`})
+	if err != nil {
+		log.Fatal("Mount failed:", err)
+	}
+	err = mp.BlockTillDone()
+	if err != nil {
+		log.Println("Filesystem exit:", err)
+	}
 	return nil
 }
