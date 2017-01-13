89c127236a3a7d67d59e504fb85c7b6265f359c3
Hector Paz <hector.paz@clicksandbricks.pe>
2017-01-13 12:02:54 -0500

    new osxfuse version

--- codepicnic-post.sh
+++ codepicnic-post.sh
@@ -5,10 +5,10 @@ echo "mkdir" >> /tmp/codepicnic_postinstall.log
 sudo mkdir /usr/local/codepicnic 
 sudo mv codepicnic.png /usr/local/codepicnic/codepicnic.png >> /tmp/codepicnic_postinstall.log
 sudo mv codepicnic-uninstall.sh /usr/local/codepicnic/uninstall.sh >> /tmp/codepicnic_postinstall.log
-sudo hdiutil attach -mountpoint /private/tmp/osxfuse osxfuse-3.4.2.dmg >> /tmp/codepicnic_postinstall.log
+sudo hdiutil attach -mountpoint /private/tmp/osxfuse osxfuse-3.5.4.dmg >> /tmp/codepicnic_postinstall.log
 cd /tmp/osxfuse/Extras
 sudo ls >> /tmp/codepicnic_postinstall.log
-sudo installer -pkg  "FUSE for macOS 3.4.2.pkg" -target "/" >> /tmp/codepicnic_postinstall.log
+sudo installer -pkg  "FUSE for macOS 3.5.4.pkg" -target "/" >> /tmp/codepicnic_postinstall.log
 cd /
 sudo hdiutil detach /tmp/osxfuse
 exit 0 # all good
