82d37c49b2b9b0271418d4f950b4c12ed3c029da
Hector Paz <hector@codepicnic.com>
2017-05-11 19:25:24 -0500

    add wix setup to jenkins

--- wix/codepicnic_wix.bat
+++ wix/codepicnic_wix.bat
@@ -1,5 +1,5 @@
 cd wix
-candle codepicnic.wxs
-light codepicnic.wixobj
-candle  -ext WixBalExtension -out codepicnic_bundle.wixobj codepicnic_bundle.wxs
-light -ext WixBalExtension -out codepicnic_setup.exe codepicnic_bundle.wixobj 
+C:\Program Files (x86)\WiX Toolset v3.10\bin\candle.exe codepicnic.wxs
+C:\Program Files (x86)\WiX Toolset v3.10\bin\light.exe codepicnic.wixobj
+C:\Program Files (x86)\WiX Toolset v3.10\bin\candle.exe  -ext WixBalExtension -out codepicnic_bundle.wixobj codepicnic_bundle.wxs
+C:\Program Files (x86)\WiX Toolset v3.10\bin\light.exe -ext WixBalExtension -out codepicnic_setup.exe codepicnic_bundle.wixobj 
