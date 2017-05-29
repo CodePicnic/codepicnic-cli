4223948ff0fd1adbbf3ac2459139fef6b0dfb823
Hector Paz <hector@codepicnic.com>
2017-05-29 16:16:24 -0500

    add notifications

--- notify_windows.go
+++ notify_windows.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"fmt"
 
+	toast "gopkg.in/toast.v1"
 )
 
 func NotifyDesktop(message string) {
-
-	//notifize.Display("CodePicnic", "Console succesfully mounted", false, share_dir_linux+"/"+notify_file)
+	notification := toast.Notification{
+		AppID:   "CodePicnic",
+		Title:   "CodePicnic",
+		Message: message,
+		//Icon:    "go.png", // This file must exist (remove this line if it doesn't)
+		/*Actions: []toast.Action{
+			{"protocol", "I'm a button", ""},
+			{"protocol", "Me too!", ""},
+		},*/
+	}
+	err := notification.Push()
+	if err != nil {
+		fmt.Println(err)
+	}
 }
