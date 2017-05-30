56ce7e18a3dc92b78b7601cbe7a610b3c54c0cfd
Hector Paz <hector@codepicnic.com>
2017-05-29 19:48:14 -0500

    add icon

--- notify_windows.go
+++ notify_windows.go
@@ -1,24 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"path/filepath"
+
+	"github.com/kardianos/osext"
 
 	toast "gopkg.in/toast.v1"
 )
 
 func NotifyDesktop(message string) {
+	cp_bin, _ := osext.Executable()
+	image_path := filepath.Dir(cp_bin)
+	image_file := image_path + "\\codepicnic.png"
+
 	notification := toast.Notification{
 		AppID:   "CodePicnic",
 		Title:   "CodePicnic",
 		Message: message,
-		//Icon:    "go.png", // This file must exist (remove this line if it doesn't)
+		Icon:    image_file, // This file must exist (remove this line if it doesn't)
 		/*Actions: []toast.Action{
 			{"protocol", "I'm a button", ""},
 			{"protocol", "Me too!", ""},
 		},*/
 	}
-	err := notification.Push()
-	if err != nil {
-		fmt.Println(err)
-	}
+	_ := notification.Push()
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
 }
